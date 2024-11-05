2caebfbb009c177b770b2f38dfa9f57018fc44ac
Alvee <M.Alvee8141@gmail.com>
2024-11-05 17:37:29 +0600

    :sparkles: env added

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,4 @@
-// dev
-export const SUPRASY_API_URL = 'http://localhost:3000';
-
-// prod
-// export const SUPRASY_API_URL = 'https://api.suprasy.com';
-
+export const SUPRASY_API_URL = import.meta.env.VITE_API_URL;
 export const SITE_URL = window.location.hostname;
-export const SUPRASY_CF_API = 'https://app-develop.suprasy.com';
-
-export const RENDER_URL = 'http://localhost:4200';
+export const SUPRASY_CF_API = import.meta.env.VITE_CF_API_URL;
+export const RENDER_URL = import.meta.env.VITE_RENDER_URL;
